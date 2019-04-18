c217e3194a1e5ea8b9682d5d04188ab49b3a4456
yjh <yjh@163.com>
2019-04-18 08:02:59 +0800

    加入购物车

--- wenxiweb/src/main/java/com/youzhong/controller/CartController.java
+++ wenxiweb/src/main/java/com/youzhong/controller/CartController.java
@@ -43,70 +43,70 @@ public class CartController {
     public String insertGoods(HttpSession session, HttpServletRequest request, Goods goods) {//登陆直接添加
         User user = (User) session.getAttribute("user");
         Cookie[] cookies = request.getCookies();
-        boolean flag=false;//判断是否有cookie
-        String num="";
+        boolean flag = false;//判断是否有cookie
+        String num = "";
         if (cookies != null) {//判断cookie有没有数据
             for (int i = 0; i < cookies.length; i++) {
                 String name = cookies[i].getName();
                 //判断名字是否一致
                 if (goods.getId().equals(name)) {
-                    flag=true;
-                     num = cookies[i].getValue();
+                    flag = true;
+                    num = cookies[i].getValue();
 
                 }
 
             }
         }
 
-        if(flag){//判断客户端是否有cookie
-            int radisNum=0;
-           // String count = goodsService.getgoodsId(user.getId(), goods.getId());
+        if (flag) {//判断客户端是否有cookie
+            int radisNum = 0;
+            // String count = goodsService.getgoodsId(user.getId(), goods.getId());
             Set<Goods> objects = goodsService.allValue(user.getId());
-            boolean result=false;
-            for (Goods goods1:objects){
-                if(goods1.getId().equals(goods.getId())){
-                    radisNum=   goods1.getNum();
-                    result=true;
+            boolean result = false;
+            for (Goods goods1 : objects) {
+                if (goods1.getId().equals(goods.getId())) {
+                    radisNum = goods1.getNum();
+                    result = true;
                     break;
                 }
             }
             //新数据
-            int xin=goods.getNum().intValue();
+            int xin = goods.getNum().intValue();
             //客户端
-            int ke=Integer.parseInt(num);
+            int ke = Integer.parseInt(num);
             //数据库
-            int newNum=0;
-            if(radisNum!=0){
-                newNum+=xin+ke+radisNum;
-            }else{
-                newNum+=xin+ke;
+            int newNum = 0;
+            if (radisNum != 0) {
+                newNum += xin + ke + radisNum;
+            } else {
+                newNum += xin + ke;
             }
 
-             if(result){//判断redis是否有数据(加上新的数量和客户端的数量)
-                 goodsService.insert(user.getId(), goods.getId(), newNum);
-             }else {//不等于空（加上数据库和客户端和新增的 ）
-                 goodsService.insert(user.getId(), goods.getId(), newNum);
-             }
-        }else{//客户端没有cookie
-            int radisNum=0;
-           // String count = goodsService.getgoodsId(user.getId(), goods.getId());
+            if (result) {//判断redis是否有数据(加上新的数量和客户端的数量)
+                goodsService.insert(user.getId(), goods.getId(), newNum);
+            } else {//不等于空（加上数据库和客户端和新增的 ）
+                goodsService.insert(user.getId(), goods.getId(), newNum);
+            }
+        } else {//客户端没有cookie
+            int radisNum = 0;
+            // String count = goodsService.getgoodsId(user.getId(), goods.getId());
             Set<Goods> objects = goodsService.allValue(user.getId());
-            boolean result=false;
-            for (Goods goods1:objects){
-                if(goods1.getId().equals(goods.getId())){
-                    radisNum=   goods1.getNum();
-                    result=true;
+            boolean result = false;
+            for (Goods goods1 : objects) {
+                if (goods1.getId().equals(goods.getId())) {
+                    radisNum = goods1.getNum();
+                    result = true;
                     break;
                 }
             }
-            if(result){//判断redis是否有数据(此时客户端没有数据)，加数据库和新增的
-                  int r=goods.getNum().intValue();
-                  if(radisNum!=0){
-                      r+=radisNum;
-                  }
+            if (result) {//判断redis是否有数据(此时客户端没有数据)，加数据库和新增的
+                int r = goods.getNum().intValue();
+                if (radisNum != 0) {
+                    r += radisNum;
+                }
 
                 goodsService.insert(user.getId(), goods.getId(), r);
-            }else {
+            } else {
                 goodsService.insert(user.getId(), goods.getId(), goods.getNum());
             }
         }
